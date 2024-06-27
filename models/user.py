a672a23e95cc71008f7f937104b94f7dd59b7d8e
KCUBE21 <138729734+KCUBE21@users.noreply.github.com>
2024-06-27 01:18:53 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the User class."""
-from models.base_model import BaseModel,Base
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
@@ -11,39 +11,8 @@ class User(BaseModel):
         first_name (str): The first name of the user.
         last_name (str): The last name of the user.
     """
-    
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        _password = Column('password',
-                           String(128),
-                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
-    # else:
-    #     email = ""
-    #     _password = ""
-    #     first_name = ""
-    #     last_name = ""
 
-
-    # def __init__(self, *args, **kwargs):
-    #     """initializes user"""
-    #     super().__init__(*args, **kwargs)
-    
-    # @property
-    # def password(self):
-    #     return self._password
-    
-    # @password.setter
-    # def password(self, pwd):
-    #     """hashing password values"""
-    #     self._password = pwd
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
