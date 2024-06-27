bb2bd57a4b4ec30109ff7ad39be85a87b64faec8
KCUBE21 <138729734+KCUBE21@users.noreply.github.com>
2024-06-27 00:47:58 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the User class."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel,Base
 
 
 class User(BaseModel):
@@ -11,8 +11,26 @@ class User(BaseModel):
         first_name (str): The first name of the user.
         last_name (str): The last name of the user.
     """
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    
+ _if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128),
+                       nullable=False)
+        _password = Column('password',
+                           String(128),
+                           nullable=False)
+        first_name = Column(String(128),
+                            nullable=True)
+        last_name = Column(String(128),
+                           nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
