8176684deae90836cf6ce4207ee6754a6422a7e5
KCUBE21 <138729734+KCUBE21@users.noreply.github.com>
2024-06-27 00:06:08 +0000

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""
-Defines the FileStorage class.
-"""
-
+"""This is the file storage class for AirBnB"""
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Place
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+import shlex
+
 
 class FileStorage:
-    """Represents an abstracted storage engine for managing object data.
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
     Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
+        __file_path: path to the JSON file
+        __objects: objects will be stored
     """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-       """returns a dictionary
+        """returns a dictionary
         Return:
             returns a dictionary of __object
         """
@@ -40,59 +39,42 @@ class FileStorage:
             return self.__objects
 
     def new(self, obj):
-        """Adds a new object to __objects with key '<obj_class_name>.id'.
+        """sets __object to given obj
         Args:
-            obj (BaseModel): The object to add.
+            obj: given object
         """
-        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[obj_key] = obj
-        self.save()  # Save the updated __objects immediately
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to JSON format and saves it to __file_path."""
-        serialized_objects = {
-            key: value.to_dict() for key, value in self.__objects.items()
-        }
-        with open(self.__file_path, "w") as file:
-            json.dump(serialized_objects, file)
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Deserializes JSON data from __file_path and populates __objects."""
+        """serialize the file path to JSON file path
+        """
         try:
-            with open(self.__file_path, "r") as file:
-                loaded_data = json.load(file)
-                for key, value in loaded_data.items():
-                    cls_name = value["__class__"]
-                    del value["__class__"]
-                    self.new(eval(cls_name)(**value))
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
-            return
-
-    def test_file_path(self):
-        """Test method to meet requirement."""
-        self.__file_path = "OK"
-
-    def test_all(self):
-        """Test method to meet requirement."""
-        pass
-
-    def test_new(self):
-        """Test method to meet requirement."""
-        pass
-
-    def test_save(self):
-        """Test method to meet requirement."""
-        pass
-
-    def test_reload(self):
-        """Test method to meet requirement."""
-        pass
+            pass
 
     def delete(self, obj=None):
-            """ delete an existing element
-            """
-            if obj:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                del self.__objects[key]
+        """ delete an existing element
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
 
-
+    def close(self):
+        """ calls reload()
+        """
+        self.reload()
