ecb9e215c53745e49309dc909b6eebc94746fc16
KCUBE21 <138729734+KCUBE21@users.noreply.github.com>
2024-06-27 00:17:43 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-"""
-model amenity class
-"""
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
 
-class Amenity(BaseModel):
-    """Amenity class that inherits from BaseModel"""
-    name = ""
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128),
+                      nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
